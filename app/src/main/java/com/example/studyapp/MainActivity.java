d6d6bda9c4616cc514a3e899583b6cff1016a9cb
lihao <lihao@cheyipai.com>
2020-05-13 16:17:29 +0800

    创建List模型

--- app/src/main/java/com/example/studyapp/MainActivity.java
+++ app/src/main/java/com/example/studyapp/MainActivity.java
@@ -4,11 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        LinkedList<String> linkedList=new LinkedList<>();
+//        linkedList.get();
+        ArrayList<String> arrayList=new ArrayList<>();
+//        arrayList.get()
+
+
+
+
+
     }
 }
